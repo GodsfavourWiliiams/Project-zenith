87a426873023cc2263cf1914de3230c899716850
Godsfavour Williams <godzfeivvur@gmail.com>
2023-07-02 11:00:05 +0100

    feat: implement goggle verify ownership tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import { supabase } from './../lib/supabaseClient';
+import Head from 'next/head';
 import Home from '../views/Home';
 
 export default function Index({ countries, date }) {
-  return <Home countries={countries} date={date} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name='google-site-verification'
+          content='1sh6t8QRUBfF9WzSoGxTch1ilV37MQvTE9h-Yu-rV9M'
+        />
+      </Head>
+      <Home countries={countries} date={date} />
+    </>
+  );
 }
 
 export async function getServerSideProps() {
